c83387f2eea1049b5bcdbe138462945ef7083f25
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-28 20:55:33 -0300

    Implementada Autenticação e Autorização com Identity

--- eAgenda.WebApp/Program.cs
+++ eAgenda.WebApp/Program.cs
@@ -11,6 +11,7 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddCamadaInfraestruturaEmOrm(builder.Configuration);
+        builder.Services.AddIdentityProviderConfig();
 
         builder.Services.AddControllersWithViews();
 
@@ -30,6 +31,7 @@ public class Program
 
         app.UseRouting();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllerRoute(
