a0087566024bb2975f29563dcb6ca2de7d9e5ce9
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-11-25 16:13:09 -0300

    Adicionados testes de categoria

---
+++ eAgenda.Testes.Interface/ModuloCategoria/CategoriaInterfaceTestes.cs
@@ -0,0 +1,77 @@
+﻿using eAgenda.Testes.Interface.Compartilhado;
+using OpenQA.Selenium;
+
+namespace eAgenda.Testes.Interface.ModuloCategoria;
+
+[TestClass]
+[TestCategory("Testes de Interface de Categoria")]
+public class CategoriaInterfaceTestes : TestFixture
+{
+    [TestMethod]
+    public void Deve_CadastrarCategoria_Corretamente()
+    {
+        // Arranjo
+        RegistrarEAutenticarUsuario();
+
+        // Ação
+        CadastrarCategoriaPadrao();
+
+        // Asserção
+        webDriverWait?.Until(d => d.Title.Contains("Visualização de Categorias"));
+        webDriverWait?.Until(d => d.PageSource.Contains("Mercado"));
+    }
+
+    [TestMethod]
+    public void Deve_EditarCategoria_Corretamente()
+    {
+        // Arranjo
+        RegistrarEAutenticarUsuario();
+
+        CadastrarCategoriaPadrao();
+
+        EsperarPorElemento(By.CssSelector("a[data-se=btnEditar]"))
+            .Click();
+
+        // Ação
+        EsperarPorElemento(By.CssSelector("input[data-se=inputTitulo]"))
+            .SendKeys(" Editado");
+
+        EsperarPorElemento(By.CssSelector("button[data-se=btnConfirmar]"))
+            .Click();
+
+        // Asserção
+        webDriverWait?.Until(d => d.Title.Contains("Visualização de Categorias"));
+        webDriverWait?.Until(d => d.PageSource.Contains("Mercado Editado"));
+    }
+
+    [TestMethod]
+    public void Deve_ExcluirCategoria_Corretamente()
+    {
+        // Arranjo
+        RegistrarEAutenticarUsuario();
+
+        CadastrarCategoriaPadrao();
+
+        EsperarPorElemento(By.CssSelector("a[data-se=btnExcluir]"))
+            .Click();
+
+        // Ação
+        EsperarPorElemento(By.CssSelector("button[data-se=btnConfirmar]"))
+            .Click();
+
+        // Asserção
+        webDriverWait?.Until(d => d.Title.Contains("Visualização de Categorias"));
+        webDriverWait?.Until(d => !d.PageSource.Contains("Mercado"));
+    }
+
+    public static void CadastrarCategoriaPadrao()
+    {
+        NavegarPara("/categorias/cadastrar");
+
+        EsperarPorElemento(By.CssSelector("input[data-se=inputTitulo]"))
+            .SendKeys("Mercado");
+
+        EsperarPorElemento(By.CssSelector("button[data-se=btnConfirmar]"))
+            .Click();
+    }
+}
